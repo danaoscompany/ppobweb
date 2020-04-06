ef1dba04d98807b59320c676ebc6895f7416ecb0
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-06 14:04:53 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,22 @@
 
 class User extends CI_Controller {
   
+  public function get_transaction_n() {
+    $phone = $this->input->post('phone');
+    $nominal = intval($this->input->post('nominal'));
+    $transaction_n = 0;
+    $transactions = $this->db->get_where('transactions', array(
+        'phone' => $phone,
+        'nominal' => $nominal
+      ))->result_array();
+    if (sizeof($transactions) > 0) {
+      $transaction = $transactions[sizeof($transactions)-1];
+      echo $transaction['transaction_n'];
+    } else {
+      echo 0;
+    }
+  }
+  
   public function purchase() {
     $userID = intval($this->input->post('user_id'));
     $phone = $this->input->post('phone');
