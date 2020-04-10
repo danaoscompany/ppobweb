c8524ec3c3ceec523693e23e205cab8c58af5083
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-10 23:06:56 +0700

    a

---
+++ application/controllers/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+class Admin extends CI_Controller {
+  
+  public function get_users() {
+    $start = intval($this->input->post('start'));
+    $length = intval($this->input->post('length'));
+    $this->db->limit($start, $length);
+    echo json_encode($this->db->get('users'));
+  }
+}
