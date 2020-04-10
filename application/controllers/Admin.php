17cc9aecc18aaaab1e9a944e87e3e1c8c7bf7e28
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-11 00:12:33 +0700

    a

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,4 +8,25 @@ class Admin extends CI_Controller {
     $this->db->limit($length, $start);
     echo json_encode($this->db->get('users')->result_array());
   }
+  
+  public function save_user() {
+    $userID = intval($this->input->post('user_id'));
+    $name = $this->input->post('name');
+    $email = $this->input->post('email');
+    $password = $this->input->post('password');
+    $phone = $this->input->post('phone');
+    $pin = $this->input->post('pin');
+    $city = $this->input->post('city');
+    $accessCode = $this->input->post('access_code');
+    $this->db->where('id', $userID);
+    $this->db->update('users', array(
+      'name' => $name,
+      'email' => $email,
+      'password' => $password,
+      'phone' => $phone,
+      'pin' => $pin,
+      'city' => $city,
+      'access_code' => $accessCode
+    ));
+  }
 }
