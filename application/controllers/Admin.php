e9b5cad5e096b320fa6844d5399e0b370400d39a
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-11 10:37:26 +0700

    a

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,10 +12,10 @@ class Admin extends CI_Controller {
         $this->load->library('upload', $config);
 
         if ($this->upload->do_upload('file')) {
-            echo "Upload success: " . json_encode($this->upload->data());
-            /*$this->db->insert('banners', array(
-              'img' => $this->upload->data()
-            ));*/
+            echo "Upload success: " . $this->upload->data()['file_name'];
+            $this->db->insert('banners', array(
+              'img' => $this->upload->data()['file_name']
+            ));
         } else {
           echo "Upload failed: " . json_encode($this->upload->display_errors());
         }
