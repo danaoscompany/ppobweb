5cab22fad48416dcda46fbcf6dfd002e1e1034bd
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-04-16 13:43:27 +0700

    a

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,6 +1,13 @@
 <?php
 
 class Admin extends CI_Controller {
+  
+  public function get_news() {
+    $start = intval($this->input->post('start'));
+    $length = intval($this->input->post('length'));
+    $this->db->limit($length, $start);
+    echo json_encode($this->db->get('news')->result_array());
+  }
 
   public function add_news() {
     $title = $this->input->post('title');
